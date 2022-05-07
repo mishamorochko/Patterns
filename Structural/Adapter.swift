bea3190c57a2e2fda38c14f3730815a0df8f6e31
mikhail <Morochko2014@gmail.com>
2022-05-07 15:52:07 +0300

    add adapter

---
+++ Structural/Adapter.swift
@@ -0,0 +1,33 @@
+// MARK: - Adapter
+class FacebookAuthSDK {
+    func presentAuthFlow() { print("FacebookAuthSDK present auth flow.") }
+}
+
+class TwitterAuthSDK {
+    func showAuth() { print("TwitterAuthSDK show auth.") }
+}
+
+protocol AuthService {
+    func presentAuthFlow()
+}
+
+extension FacebookAuthSDK: AuthService { }
+
+extension TwitterAuthSDK: AuthService {
+    func presentAuthFlow() {
+        showAuth()
+    }
+}
+
+class App {
+    func main() {
+        startAuthorization(authSDK: TwitterAuthSDK())
+    }
+
+    private func startAuthorization(authSDK: AuthService) {
+        authSDK.presentAuthFlow()
+    }
+}
+
+let app = App()
+app.main()
