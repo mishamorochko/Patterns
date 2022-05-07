e1efb0bb6d4e9cc3aabf570291d8ae94681e5768
mikhail <Morochko2014@gmail.com>
2022-05-07 10:58:53 +0300

    Factory Method

---
+++ Factory Method.swift
@@ -0,0 +1,64 @@
+// Factory method
+
+protocol Transport {
+    func deliver()
+}
+
+protocol Logistic {
+    func createTransport() -> Transport
+}
+
+class SeaLogistic: Logistic {
+    func createTransport() -> Transport {
+        return SeaTransport()
+    }
+}
+
+class SeaTransport: Transport {
+    func deliver() {
+        print("SeaTransport deliver.")
+    }
+}
+
+class RoadLogistic: Logistic {
+    func createTransport() -> Transport {
+        return RoadTransport()
+    }
+}
+
+class RoadTransport: Transport {
+    func deliver() {
+        print("RoadTransport deliver.")
+    }
+}
+
+class FlyLogistic: Logistic {
+    func createTransport() -> Transport {
+        return FlyTransport()
+    }
+}
+
+class FlyTransport: Transport {
+    func deliver() {
+        print("FlyTransport deliver.")
+    }
+}
+
+class App {
+    func main() {
+        deliver(logistic: FlyLogistic())
+        getCoffee(coffeeShop: ItalianCoffeeShop())
+    }
+
+    private func deliver(logistic: Logistic) {
+        logistic.createTransport().deliver()
+    }
+
+    private func getCoffee(coffeeShop: CoffeeShop) {
+        let coffee = coffeeShop.createCoffee()
+        coffee.pourIntoCup()
+    }
+}
+
+let app = App()
+app.main()
