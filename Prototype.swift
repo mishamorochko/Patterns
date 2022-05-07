9c4fd793af4487337fb797a86fc88fe3d737e9f8
mikhail <Morochko2014@gmail.com>
2022-05-07 11:46:24 +0300

    Prototype

---
+++ Prototype.swift
@@ -0,0 +1,24 @@
+// Prototype
+/// The Prototype pattern is available in Swift out of the box with a NSCopying interface.
+
+import Foundation
+class User: NSCopying {
+
+    var name: String
+    private let age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    func copy(with zone: NSZone? = nil) -> Any {
+        let copy = User(name: name, age: age)
+        return copy
+    }
+}
+
+let user1 = User(name: "Mikhail", age: 22)
+let user2 = user1.copy() as! User
+
+user1.name = "Oleg"
+print(user2.name)
